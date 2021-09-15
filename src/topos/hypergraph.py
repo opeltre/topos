1377ecb17690a04a3944611f81e5530bf7a637e4
Olivier Peltre <opeltre@gmail.com>
2021-09-15 16:59:02 +0200

    Fix hypergraph methods .above, .below, .intercone

--- src/topos/hypergraph.py
+++ src/topos/hypergraph.py
@@ -2,25 +2,29 @@ from .set import Set
 from .tensor import Tensor
 from itertools import product
 
+
+def toSet (a):
+    return a if isinstance(a, Set) else Set(a)
+
 class Hypergraph (Set): 
     """
     Hypergraphs 
     """
     def __init__(self, elems, sep=',', **kwargs):
+
         if type(elems) == str:
             elems = elems.split(sep)
+        
         K = [Set(e) if type(e) == str else e for e in elems]
+
+        self.chains = [(a, b) for a, b in product(K, K) if a > b]\
+                      if "chains" not in kwargs else kwargs["chains"]
 
         super().__init__(K, sep)
         
-        self.chains =   \
-            [(a, b) for a, b in product(K, K) if a > b]\
-            if "chains" not in kwargs else kwargs["chains"]
-
-        self._below, self._above =  \
-            {a: [] for a in self},  \
-            {b: [] for b in self}
-
+        self._below = {a : [] for a in self}
+        self._above = {b : [] for b in self}
+    
         for (a, b) in self.chains:
             self._below[a] += [b]
             self._above[b] += [a]
@@ -35,20 +39,20 @@ class Hypergraph (Set):
     def below (self, a, strict=1):
         if not strict: 
             yield a
-        for b in self._below[a]:
+        for b in self._below[toSet(a)]:
             yield b
 
     def above (self, b, strict=1):
         if not strict: 
             yield b
-        for a in self._above[b]:
+        for a in self._above[toSet(b)]:
             yield a
 
     def between (self, a, c): 
-        return (b for b in self.below(a) if b > c)
+        return (b for b in self.below(a) if b > toSet(c))
 
     def intercone (self, a, c, strict=1):
-        return (b for b in self.below(a) if not b <= c)
+        return (b for b in self.below(a) if not b <= toSet(c))
 
     def add(self, elem): 
         super().add(Set((elem)))
