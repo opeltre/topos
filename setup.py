bc2d817179ab88beffdeb1e57eaa9e95c44790be
Olivier Peltre <opeltre@gmail.com>
2021-10-09 11:06:30 +0200

    add setup.py and move examples

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+with open('./requirements.txt', 'r') as f:
+    requirements = f.read().strip().split('\n')
+
+setup(
+    name    ='topos',
+    version ='0.1',
+    description ="Statistics and Topology",
+    author      ="Olivier Peltre",
+    author_email='opeltre@gmail.com',
+    url     ='https://github.com/opeltre/topos',
+    license ='MIT',
+    install_requires=requirements,
+    packages = ['topos', 
+                'topos.base', 
+                'topos.core',
+                'topos.domain']
+)
