dcf6e759b859ff4390ef1cdc5b733b2f9c524535
Olivier Peltre <opeltre@gmail.com>
2022-08-03 13:15:06 +0200

    add io.plot for Complex instances

--- topos/io/__init__.py
+++ topos/io/__init__.py
@@ -1,3 +1,5 @@
 from .io_utils import readKey, readFunctor, readTensor, showTensor, alignString
 
 from .exceptions import ToposError, FieldError, VectError, LinearError
+
+from .plot import plot_graph, plt
