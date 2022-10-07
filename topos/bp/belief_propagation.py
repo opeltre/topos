8ca6e5d987146f16119c1127b63e019304ebbeed
Olivier Peltre <opeltre@gmail.com>
2022-10-07 16:53:15 +0200

    refactor exp_, _ln ... using @graded_map

--- topos/bp/belief_propagation.py
+++ topos/bp/belief_propagation.py
@@ -3,55 +3,55 @@ import torch
 
 from topos.base import Nerve
 
+
+def graded_map(method):
+    def run(self, x, *args, **kwargs):
+        if isinstance(x, fp.Tensor):
+            return method(self, x.degree, *args, **kwargs)(x)
+        return method(self, x, *args, **kwargs)
+    return run
+
 class Network(Nerve):
     
-    def exp_(self, x, beta=1):
+    @graded_map
+    def exp_(self, d, beta=1):
         """
         Exponential map from energies to unnormalized Gibbs densities. 
 
         Returns `torch.exp(- beta * x)`. 
         """
-        if isinstance(x, int):
-            src = self.Field(x)
-            f = fp.Arrow(src, src)(lambda t: torch.exp(- beta * t.data))
-            f.__name__ = "(e-)"
-            return f
-        elif isinstance(x, fp.Tensor):
-            src = self.Field(x.degree)
-            return src(torch.exp(-beta * x.data))
+        src = self.Field(d)
+        f = fp.Arrow(src, src)(lambda t: torch.exp(- beta * t.data))
+        f.__name__ = "(e-)"
+        return f
 
-    def _ln(self, x, beta=1):
+    @graded_map
+    def _ln(self, d, beta=1):
         """
         Logarithmic map from densities to energies.
 
         Returns `- torch.log(x) / beta`. 
         """
-        if isinstance(x, int):
-            src = self.Field(x)
-            f = fp.Arrow(src, src)(lambda t: - torch.log(t.data) / beta)
-            f.__name__ = "(-ln)"
-            return f
-        elif isinstance(x, fp.Tensor):
-            src = self.Field(x.degree)
-            return src(- torch.log(x.data) / beta)
+        src = self.Field(d)
+        f = fp.Arrow(src, src)(lambda t: - torch.log(t.data) / beta)
+        f.__name__ = "(-ln)"
+        return f
 
-    def gibbs(self, x, beta=1):
+    @graded_map
+    def gibbs(self, d, beta=1):
         """
         Normalized Gibbs density. 
 
         Defined by `p(x) = exp(- beta * x) / Z` (sometimes called a softmin).
         """
-        if isinstance(x, int):
-            src = self.Field(x)
-            def p(H):
-                y = self.exp_(x, beta)(H)
-                Z = self.to_scalars(y)
-                return y / self.from_scalars(Z)
-            f = fp.Arrow(src, src)(p)
-            f.__name__ = "gibbs"
-            return f
-        elif isinstance(x, fp.Tensor) and 'degree' in dir(x):
-            return self.gibbs(x.degree, beta)(x)
+        src = self.Field(d)
+        def p(H):
+            y = self.exp_(d, beta)(H.data)
+            Z = self.to_scalars(y)
+            return y / self.from_scalars(Z)
+        f = fp.Arrow(src, src)(p)
+        f.__name__ = "gibbs"
+        return f
         
     def freeEnergy(self, x):
         pass
